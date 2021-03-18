bd9dd0e995b37871a004fa955a5c189ab8e61619
Fabio Petrone <fa610movic90@gmail.com>
2021-03-18 18:03:49 +0100

    fine prove

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Beer;
 
 class TestController extends Controller
@@ -14,7 +15,7 @@ class TestController extends Controller
     // $beers = Beer::where([['colour', '=', 'red'],
     // ['Strength', '=', 'strong'],])->get();
     $beers =Beer::whereBetween('price', [4, 5])->orderBy('price', 'desc')->get();
-    // $beers = Beer::groupBy('colour')->having('price','>',4)->get();
+     // $beers = DB::table('beers')->groupBy('id')->having('colour', '=', 'red')->get();
     // dd($beers);
     // $b=Book::where('price','!=','9.50')->limit(1)->get();
     return view('beers.index',compact('beers'));
